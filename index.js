497c082a9013ce26116117bfe17c93f87ce710f4
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2017-12-21 21:43:48 +0100

    add basic chart rendering

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const _ = require('underscore');
 const split = require('split');
+const generate = require('node-chartist');
 
 const regex = new RegExp("[^\\n\\r\\t ]+",'g')
 const test_file = 'logs/Stck1-21122017-1415Uhr.txt'; // todo: command line
+const test_output_file = 'test.html';
 
 const timestamp_index = 0;
 const socket_index = 1;
@@ -45,18 +47,15 @@ const power_index = 6;
  */
 const convertStreamToJSON = (outData) => {
   let result = {
-    label: outData[0][socket_index],
-    data: []
+    labels: [],
+    series: [[]]
   };
   _.each(outData, od => {
-    result.data.push({
-      timestamp: od[timestamp_index],
-      energy: od[energy_index],
-      power: od[power_index]
-    });
+    result.labels.push(od[timestamp_index]);
+    result.series[0].push(od[energy_index]);
   });
 
-  console.log(result);
+  return result;
 }
 
 const readFile = (inPath) => {
@@ -78,8 +77,30 @@ const readFile = (inPath) => {
     });
 
   readStream.on('end', () => {
-    convertStreamToJSON(outData);
+    writeDiagram(convertStreamToJSON(outData));
   });
+}
+
+const writeDiagram = (dataObject) => {
+  const options = {width: 4000, height: 2000};
+
+  /**const exampleData = {
+    labels: ['a','b','c','d','e'],
+    series: [
+      [1, 2, 3, 4, 5],
+      [3, 4, 5, 6, 7]
+    ]
+  };**/
+
+  generate('line', options, dataObject).then(chartDiv => {
+    // write to ouput file
+    fs.writeFile(test_output_file, chartDiv, (err) => {
+      if(err) {
+          return console.log(err);
+      }
+      console.log("The file was saved!");
+    }); 
+  }) 
 }
 
 readFile(test_file);
