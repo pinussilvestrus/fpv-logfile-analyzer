a54cfd849abf682bbc4abb8aa5dabea307f607bc
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2018-01-06 12:12:38 +0100

    adding handlebars rendering (fix #1) add simple web server (fix #4)

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # fpv-logfile-analyzer
 Script for automatically analyzing the smart-meter logfiles (Forschungsprojekt Virtualisierung HS-Harz)
+
+## Requirements
+
+* node.js
+
+## Setup
+
+1. Clone directory into local folder
+2. Go into the cloned folder and enter `npm install`
+
+## Run
+
+1. Go into project folder
+2. run `node index.js` for generating the chart
+3. run `npm start` for serving the generated chart on http://localhost:8000/
+
--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const fs = require('fs');
 const _ = require('underscore');
 const split = require('split');
-const generate = require('node-chartist');
+const renderChart = require('node-chartist');
+const handlebars = require('handlebars');
+const promisify = require('es6-promisify');
+const path = require('path');
 
 const regex = new RegExp("[^\\n\\r\\t ]+",'g');
-
-// options todo: command line
-const test_file = 'logs/Stck1-21122017-1415Uhr.txt'; 
-const test_output_file = 'test.html';
-const data_factor = 100; // indicates, how much data is retrieved from files, value = 2 => every 2nd..
+const templateFile = './template.hbs';
+// options defaults todo: command line
+const inputFile = 'logs/Stck1-21122017-1415Uhr.txt'; 
+const outputFile = 'index.html';
+const dataFactor = 100; // indicates, how much data is retrieved from files, value = 2 => every 2nd..
 const fetchEnergy = false;
 
-const timestamp_index = 0;
-const socket_index = 1;
-const energy_index = 4;
-const power_index = 6;
+const timestampIndex = 0;
+const socketIndex = 1;
+const energyIndex = 4;
+const powerIndex = 6;
 
 /**
  * 
@@ -55,13 +58,13 @@ const convertStreamToJSON = (outData) => {
     series: [[]]
   };
   _.each(outData, (od, index) => {
-    if (index % data_factor == 0) {
-      result.labels.push(od[timestamp_index]);
-      result.series[0].push(od[fetchEnergy ? energy_index: power_index]);
+    if (index % dataFactor == 0) {
+      result.labels.push(od[timestampIndex]);
+      result.series[0].push(od[fetchEnergy ? energyIndex: powerIndex]);
     }
   });
 
-  console.log(`${result.labels.length} from overall ${outData.length} data points were retrieved for chart (factor ${data_factor})`);
+  console.log(`${result.labels.length} from overall ${outData.length} data points were retrieved for chart (factor ${dataFactor})`);
 
   return result;
 }
@@ -100,15 +103,22 @@ const writeDiagram = (dataObject) => {
     ]
   };**/
 
-  generate('line', options, dataObject).then(chartDiv => {
-    // write to ouput file
-    fs.writeFile(test_output_file, chartDiv, (err) => {
-      if(err) {
-          return console.log(err);
-      }
-      console.log(`The chart was rendered in ${test_output_file}!`);
-    }); 
-  }) 
+  renderChart('line', options, dataObject).then(chartDiv => {
+    // render to output-file with handlebars
+    return promisify(fs.readFile)(path.join(__dirname, templateFile)).then(res => {
+      let source = res.toString();
+        let template = handlebars.compile(source);
+        let html = template({
+          chartDiv: chartDiv
+        });
+
+        return promisify(fs.writeFile)(outputFile, html).then(res => {
+          console.log(`The chart was rendered in ${outputFile}!`);
+        }).catch(err => {
+          console.log(err);
+        });
+      });
+    });
 }
 
-readFile(test_file);
+readFile(inputFile);
