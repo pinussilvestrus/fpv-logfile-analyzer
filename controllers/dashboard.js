a4e4aedcfede9708224cbee6d416f250641ef389
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2018-01-29 13:45:25 +0100

    move calculations to helper

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -18,17 +18,6 @@ const markSelected = (options, value, key) => {
     });
 }
 
-const calculateUsedEnergyPerMinute = (measurements, id) => {
-    let result;
-    _.forEach(measurements, m => {
-        if (JSON.stringify(m._id) === JSON.stringify(id)) {
-            m.statistics.usedEnergykWh = m.statistics.usedEnergy / 1000;
-            result = Math.round((m.statistics.usedEnergykWh / m.statistics.calculatedMinutes) * 1000000) / 1000000; // 6 digits
-        }
-    });
-    return result;
-}
-
 router.get('/', function(req, res, next) {
 
     // retrieve calculation data
@@ -37,26 +26,7 @@ router.get('/', function(req, res, next) {
         measurementsModel.find({}).then(measurements => {
 
             // calculations
-            // calculation guideline: https://docs.google.com/document/d/1SF0vrBLKHBzJuAh-gRYZQlBSp0ckI7uhUETIqqISL3M/edit#
-            calculation.eComputerAltUsedEnergy = calculateUsedEnergyPerMinute(measurements, calculation.eVorher.eComputerAlt);
-            calculation.eVorherUsedEnergy = Math.round(
-                (calculation.eComputerAltUsedEnergy * calculation.eVorher.tLabore * calculation.eVorher.cComputerDurchschnitt) *
-                10000) / 10000; // 4 digits
-
-            calculation.eSteckdose1UsedEnergy = calculateUsedEnergyPerMinute(measurements, calculation.eNachher.eServerraum.eSteckdose1);
-            calculation.eSteckdose2UsedEnergy = calculateUsedEnergyPerMinute(measurements, calculation.eNachher.eServerraum.eSteckdose2);
-            calculation.eServerraumUsedEnergy = Math.round(
-                ((calculation.eSteckdose1UsedEnergy + calculation.eSteckdose2UsedEnergy) * calculation.eNachher.eServerraum.tSemester * calculation.eNachher.eServerraum.cServer) *
-                10000) / 10000; // 4 digits
-
-            calculation.eZeroClientUsedEnergy = calculateUsedEnergyPerMinute(measurements, calculation.eNachher.eZeroClient);
-            calculation.eZeroClientsUsedEnergy = Math.round(
-                (calculation.eZeroClientUsedEnergy * calculation.eNachher.tLabore * calculation.eNachher.cZeroClientsDurchschnitt) *
-                10000) / 10000; // 4 digits
-
-            calculation.eNachherUsedEnergy = Math.round(
-                (calculation.eZeroClientsUsedEnergy + calculation.eServerraumUsedEnergy) *
-                10000) / 10000; // 4 digits
+            calculation = analyzer.calculateUsedEnergy(calculation, measurements);
 
             // selection
             measurements = markSelected(measurements, calculation.eVorher.eComputerAlt, 'selectedComputerAlt');
